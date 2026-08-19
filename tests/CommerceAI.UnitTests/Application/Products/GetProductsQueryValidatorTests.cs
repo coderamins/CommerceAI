35c8c0e546134578bdc2dec4514468b93a5a2b78
raminsolhi <raminsolhi@gmail.com>
2026-08-19 23:31:55 +0330

    test: add application validation tests

--- tests/CommerceAI.UnitTests/Application/Products/GetProductsQueryValidatorTests.cs
+++ tests/CommerceAI.UnitTests/Application/Products/GetProductsQueryValidatorTests.cs
@@ -1,5 +1,39 @@
-﻿namespace CommerceAI.UnitTests.Application.Products;
+﻿using CommerceAI.Application.Features.Products.GetProducts;
+using CommerceAI.Application.Queries.Products.GetProducts;
+using FluentValidation.TestHelper;
+
+namespace CommerceAI.UnitTests.Application.Products;
 
 public class GetProductsQueryValidatorTests
 {
+    private readonly GetProductsQueryValidator _validator=new();
+
+    [Fact]
+    public void Should_have_error_when_page_size_is_greater_than_100()
+    {
+        var query = new GetProductsQuery(
+                PageNumber: 1,
+                PageSize: 101
+            );
+
+        var result =_validator.TestValidate(query);
+
+        result.ShouldHaveValidationErrorFor(x=>x.PageSize);
+    }
+
+    [Fact]
+    public void Should_have_error_when_min_price_is_greater_than_max_price()
+    {
+        var query = new GetProductsQuery(
+                PageNumber: 1,
+                PageSize: 20,
+                MinPrice: 500,
+                MaxPrice: 100
+            );
+
+        var result=_validator.TestValidate(query);
+
+        result.ShouldHaveValidationErrors();
+    }
+
 }
