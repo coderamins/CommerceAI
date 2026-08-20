8e02c6f6bc1833ccc8f327ab98b607a88a269efe
raminsolhi <raminsolhi@gmail.com>
2026-08-20 15:52:55 +0330

    feature/integration test first commit

---
+++ tests/CommerceAI.IntegrationTests/Infrastructure/DatabaseFixture.cs
@@ -0,0 +1,18 @@
+﻿namespace CommerceAI.IntegrationTests.Infrastructure;
+
+public sealed class DatabaseFixture : IAsyncLifetime
+{
+    private readonly PostgreSqlFixture _postgres;
+    public ApplicationDbContext
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
